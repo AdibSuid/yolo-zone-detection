9aa727339bf52dd793fa8b002e54c338b7198840
AdibSuid <adibcom.as@gmail.com>
2025-11-05 21:26:43 +0800

    fix the resolution issue

--- scripts/export.py
+++ scripts/export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8s.pt")
+model = YOLO("best.pt")
 
 # Export the model to OpenVINO format
 model.export(format="openvino", half=True, dynamic=True, nms=True, imgsz=(480, 640))  # Export with FP16 precision and dynamic shapes
